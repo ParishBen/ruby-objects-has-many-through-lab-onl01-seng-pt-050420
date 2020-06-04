76d874db3f0733c31cd327ab804be8324754f025
Ben Parish <parishben13@gmail.com>
2020-06-04 03:05:48 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,21 @@
+class Patient
+  @@all= []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+  def appointments
+    Appointment.all.map{|apt| apt.patient == self}
+  end
+  def doctors
+    appointments.map{|apt| apt.doctor}
+  end
+end
