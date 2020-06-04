c5a3f4ce92946f9375225f69a137e37661ce0f9b
Ben Parish <parishben13@gmail.com>
2020-06-04 03:12:45 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -11,14 +11,17 @@ class Doctor
   def self.all 
     @@doctors
   end
+  
   def appointments
     Appointment.all.select {|appt| appt.doctor == self}
   end
+ 
   def new_appointment(date, patient)
     Appointment.new(date, patient, self)
   end
+  
   def patients
-    appointments.map(&:patient)
+    appointments.map{|apt| apt.patient}
   end
 end
   
